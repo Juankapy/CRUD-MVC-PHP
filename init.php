39389043c4e8b9aa8193cdbfb79bc41bc2489538
Juank <jpereavelasco@safareyes.es>
2025-12-11 12:10:06 +0100

    Update init.php

--- init.php
+++ init.php
@@ -6,7 +6,7 @@ date_default_timezone_set('America/Sao_Paulo');
 
 // conexão com o banco de dados
 
-define('BD_SERVIDOR','fdb1034.awardspace.net    ');
+define('BD_SERVIDOR','fdb1034.awardspace.net');
 define('BD_USUARIO','4706077_exdespliegue');
 define('BD_SENHA','Sevilla1313.');
 define('BD_BANCO','4706077_exdespliegue');
