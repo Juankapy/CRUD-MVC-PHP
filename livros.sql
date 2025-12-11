1130a692810cc95986fca7f9231789aea16b5b1c
Juank <jpereavelasco@safareyes.es>
2025-12-11 12:23:31 +0100

    base de datos exportada

---
+++ livros.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: fdb1034.awardspace.net
+-- Tiempo de generación: 11-12-2025 a las 10:47:57
+-- Versión del servidor: 8.0.32
+-- Versión de PHP: 8.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `4706077_exdespliegue`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `livros`
+--
+
+CREATE TABLE `livros` (
+  `nome` varchar(255) NOT NULL,
+  `autor` varchar(255) DEFAULT NULL,
+  `quantidade` int NOT NULL,
+  `preco` varchar(255) NOT NULL,
+  `flag` tinyint(1) DEFAULT '0',
+  `DATA` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`nome`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
